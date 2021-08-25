bbf3d210e2c0594326264d09aea5ab5c0ad371eb
cgar246 <christiang3268@yahoo.com>
2021-08-25 16:44:17 -0700

    added all files and current code

---
+++ Assets/quiz.js
@@ -0,0 +1,56 @@
+// array with all the questions and ansers
+var myQuestions = [
+{
+    question: "commonly used data types DO NOT include",
+    answers: {
+    a: "strings",
+    b: "booleans",
+    c: "alerts",
+    d: "numbers"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "The condition in an if an if / else statement is enclosed with _____.",
+    answers: {
+    a: "quotes",
+    b: "curly brackets",
+    c: "parenthesis",
+    d: "square brackets"
+    },
+    correctAnswer: "c"
+},
+{
+    question: "Arrays in JavaScript can be used to store ___.",
+    answers: {
+    a: "numbers",
+    b: "other arrays",
+    c: "booleans",
+    d: "all of the above"
+    },
+    correctAnswer: "d"
+},
+{
+    question: "String values must be enclosed within ___ when being assigned to variables",
+    answers: {
+    a: "commas",
+    b: "curly brackets",
+    c: "quotes",
+    d: "paranthesis"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: {
+    a: "JavaScript",
+    b: "terminal / bash",
+    c: "for loops",
+    d: "console.log"
+    },
+    correctAnswer: "c"
+},
+];
+
+var score = 0;
+var 
